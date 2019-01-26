3c63cf61acfcc32ea9bc65f39b2830612f91de11
LUCAS DE CURTIS <lucas.de-curtis@epitech.eu>
2019-01-26 16:50:05 +0100

    modified function to fork only if file is found

--- minishell_command.c
+++ minishell_command.c
@@ -7,13 +7,26 @@
 
 #include "my.h"
 
+void display_command_errors(char *program_name, int status)
+{
+    if (my_strncmp(program_name, "./", 2) == 0)
+        wait(&status);
+    if (status == 9 || status == 134)
+        my_putstr("Segmentation fault\n");
+    else if (status != 0) {
+        my_putstr(program_name);
+        my_putstr(": Command not found.\n");
+    }
+}
+
 void do_command(char **path, char *program_name, char *const *args)
 {
     int c_pid = fork();
     int status;
     char *tmp;
 
-    if (c_pid == 0) {
+    if (c_pid == 0 && path[0] != NULL &&
+    my_strncmp(program_name, "./", 2) != 0) {
         for (int i = 0; path[i] != NULL; i++) {
             tmp = malloc(sizeof(char) * 20);
             tmp = my_strcat(path[i], "/");
@@ -21,15 +34,14 @@ void do_command(char **path, char *program_name, char *const *args)
             free(tmp);
         }
     } else if (c_pid > 0) {
-        if (wait(&status) < 0)
-            perror("wait");
+        if (path[0] != NULL && my_strncmp(program_name, "./", 2) != 0)
+            wait(&status);
         kill(c_pid, SIGKILL);
-    } else
-        perror("fork failed");
-    if (status == 139)
-        my_putstr("Command not found.\n");
-    else if (status != 0)
-        my_putstr("Segmentation Fault.\n");
+    } else {
+        if (path[0] != NULL && my_strncmp(program_name, "./", 2) != 0)
+            perror("fork failed");
+    }
+    display_command_errors(program_name, status);
 }
 
 void find_command_3(struct data data)
@@ -68,8 +80,10 @@ void find_command(struct data data)
     char pwd[128];
 
     getcwd(pwd, sizeof(pwd));
-    if (my_strcmp(data.program_name, "exit") == 0)
+    if (my_strcmp(data.program_name, "exit") == 0) {
+        my_putstr("exit\n");
         exit(0);
+    }
     if (my_strcmp(data.program_name, "pwd") == 0) {
         my_putstr(pwd);
         my_putchar('\n');
